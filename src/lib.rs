5ab919eb6a5a498b49f4023e021995fa7840b52a
McFreely <paulmcfreely@gmail.com>
2015-09-30 20:47:38 +0200

    Remove a rogue print statement.

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,49 @@
-pub mod narcissus {
-    pub fn euclidian_distance(vec_one: Vec<f32>, vec_two: Vec<f32>) -> f32 {
-        let mut result: f32 = 0.0;
+#![crate_name = "narcissus"]
 
-        for (i, j) in vec_one.iter().zip(vec_two) {
-            result += (i - j).abs().powf(2.0);
-        }
+pub fn euclidian_distance(vec_one: Vec<f32>, vec_two: Vec<f32>) -> f32 {
+    let mut result: f32 = 0.0;
 
-        result.sqrt()
+    for (i, j) in vec_one.iter().zip(vec_two) {
+        result += (i - j).abs().powf(2.0);
     }
 
-    pub fn cosine_similarity(vec_one: Vec<f32>, vec_two: Vec<f32>) -> f32 {
-        let dot_product = dot_product(&vec_one, &vec_two);
-        let magnitude = magnitude(&vec_one) * magnitude(&vec_two);
+    result.sqrt()
+}
 
-        println!("{:?}", dot_product);
-        dot_product / magnitude
+pub fn cosine_similarity(vec_one: Vec<f32>, vec_two: Vec<f32>) -> f32 {
+    let dot_product = dot_product(&vec_one, &vec_two);
+    let magnitude = magnitude(&vec_one) * magnitude(&vec_two);
+
+    dot_product / magnitude
+}
+
+pub fn jaccard_coeficient(vec_one: Vec<f32>, vec_two: Vec<f32>) -> f32 {
+    let dot_product = dot_product(&vec_one, &vec_two);
+    let magnitude_vec_one = magnitude(&vec_one);
+    let magnitude_vec_two = magnitude(&vec_two);
+
+    dot_product / ((magnitude_vec_one.abs().powf(2.0) + magnitude_vec_two.abs().powf(2.0)) - (dot_product))
+}
+
+// pub fn pearson_correlation<T>(vec_one: Vec<T>, vec_two: Vec<T>) -> f32 {
+// }
+
+// pub fn kullback_liebler_divergence<T>(vec_one: Vec<T>, vec_two: Vec<T>) -> f32 {
+// }
+
+fn dot_product(vec_one: &Vec<f32>, vec_two: &Vec<f32>) -> f32 {
+    let mut result: f32 = 0.0;
+
+    for (i, j) in vec_one.iter().zip(vec_two) {
+        result += i * j
     }
 
-    pub fn jaccard_coeficient(vec_one: Vec<f32>, vec_two: Vec<f32>) -> f32 {
-        let dot_product = dot_product(&vec_one, &vec_two);
-        let magnitude_vec_one = magnitude(&vec_one);
-        let magnitude_vec_two = magnitude(&vec_two);
+    result
+}
 
-        dot_product / ((magnitude_vec_one.abs().powf(2.0) + magnitude_vec_two.abs().powf(2.0)) - (dot_product))
-    }
-
-    // pub fn pearson_correlation<T>(vec_one: Vec<T>, vec_two: Vec<T>) -> f32 {
-    // }
-    
-    // pub fn kullback_liebler_divergence<T>(vec_one: Vec<T>, vec_two: Vec<T>) -> f32 {
-    // }
-
-    fn dot_product(vec_one: &Vec<f32>, vec_two: &Vec<f32>) -> f32 {
-        let mut result: f32 = 0.0;
-
-        for (i, j) in vec_one.iter().zip(vec_two) {
-            result += i * j
-        }
-
-        result
-    }
-
-    fn magnitude(vec: &Vec<f32>) -> f32 {
-        // The magnitude of a vector is the sqrt of its own dotproduct
-        dot_product(vec, vec).sqrt()
-    }
+fn magnitude(vec: &Vec<f32>) -> f32 {
+    // The magnitude of a vector is the sqrt of its own dotproduct
+    dot_product(vec, vec).sqrt()
 }
 
 #[cfg(test)]
